219753a5af4e6cdf83b6931454b6f119f15e5ec8
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2022-05-27 11:52:52 +0300

    initial push

---
+++ lib/luxurious_jackal_ptr3.ex
@@ -0,0 +1,18 @@
+defmodule LuxuriousJackalPtr3 do
+  @moduledoc """
+  Documentation for `LuxuriousJackalPtr3`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LuxuriousJackalPtr3.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/luxurious_jackal_ptr3_test.exs
@@ -0,0 +1,8 @@
+defmodule LuxuriousJackalPtr3Test do
+  use ExUnit.Case
+  doctest LuxuriousJackalPtr3
+
+  test "greets the world" do
+    assert LuxuriousJackalPtr3.hello() == :world
+  end
+end
