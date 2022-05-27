49dcc433f045af33e7f636d6176946bf71adb0d4
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2022-05-27 12:04:33 +0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,9 +1,9 @@
-defmodule LuxuriousJackalPtr3.MixProject do
+defmodule LuxuriousJackalPTR3.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :luxurious_jackal_ptr3,
+      app: :LuxuriousJackalPTR3,
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
@@ -14,7 +14,8 @@ defmodule LuxuriousJackalPtr3.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {LuxuriousJackalPTR3, []}
     ]
   end
 
