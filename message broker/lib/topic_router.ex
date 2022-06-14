912ac45aeda3b93eb1151d300cb133b5b237751e
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2022-06-14 14:29:46 +0300

    topic ensuring functionality

--- message broker/lib/topic_router.ex
+++ message broker/lib/topic_router.ex
@@ -16,6 +16,13 @@ defmodule TopicRouter do
     GenServer.cast(__MODULE__, {:add_topic, {topic, pid}})
   end
 
+  def send_to_topic_worker(message, topic_list, topic_worker_list) do
+    {topic_to_match, data} = message
+    matching_index = Enum.find_index(topic_list, fn topic -> topic === topic_to_match end)
+    matching_topic_worker = Enum.at(topic_worker_list, matching_index)
+    TopicWorker.receive_message(message, matching_topic_worker)
+  end
+
   def init(state) do
     {:ok, state}
   end
@@ -26,7 +33,7 @@ defmodule TopicRouter do
     topic_list = state.topic_list
     topic_worker_list = state.topic_worker_list
     if Enum.member?(topic_list, topic) do
-      send_to_topic_worker(message)
+      send_to_topic_worker(message, topic_list, topic_worker_list)
     else
       TopicSupervisor.create_worker(topic)
     end
@@ -36,8 +43,8 @@ defmodule TopicRouter do
   def handle_cast(:add_topic, {topic, pid}, state) do
     topic_worker_list = state.topic_worker_list
     topic_list = state.topic_list
-    new_topic_worker_list = [{topic, pid} | topic_worker_list]
     new_topic_list = [topic | topic_list]
+    new_topic_worker_list = [pid | topic_worker_list]
     {:noreply, %{state | topic_list: new_topic_list, topic_worker_list: new_topic_worker_list}}
   end
 
--- message broker/lib/topic_worker.ex
+++ message broker/lib/topic_worker.ex
@@ -2,16 +2,26 @@ defmodule TopicWorker do
   use GenServer
   require Logger
 
+#  TODO add state
   def start_link(topic) do
     GenServer.start_link(__MODULE__, %{}, name: String.to_atom("#{topic}Worker"))
+  end
+
+  defp extract_topic(topic_worker_list) do
+    
+  end
+
+  def receive_message(message, topic_worker_pid) do
+    {topic_to_match, data} = message
+    GenServer.cast(topic_worker_pid, {:message, message})
   end
 
   def init(state) do
     {:ok, state}
   end
 
-#  def handle_cast({:message, message}, state) do
-#    Logger.info("message: #{inspect(message)}")
-#    {:noreply, state}
+  def handle_cast({:message, message}, state) do
+    Logger.info("message: #{inspect(message)}")
+    {:noreply, state}
   end
 end
